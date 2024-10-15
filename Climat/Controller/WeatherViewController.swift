ce697cb94224839f722cf07eb004af12cdbec8e2
JuliaKagirova <kagirova.y@inbox.ru>
2024-10-15 12:01:19 +0300

    Initial Commit

---
+++ Climat/Controller/WeatherViewController.swift
@@ -0,0 +1,270 @@
+//
+//  ViewController.swift
+//  Climat
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController {
+    
+//    @IBOutlet weak var conditionImageView: UIImageView!
+//    @IBOutlet weak var temperatureLabel: UILabel!
+//    @IBOutlet weak var cityLabel: UILabel!
+//    @IBOutlet weak var searchTextField: UITextField!
+    
+    //MARK: - Properties
+ 
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+    
+    //MARK: - Private Properties
+    
+    private lazy var conditionImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.tintColor = .black
+        return view
+    }()
+    
+    private lazy var bgView: UIImageView = {
+        var view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "background")
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private lazy var temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.text = "21"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 46, weight: .heavy)
+        return label
+    }()
+    
+    private lazy var cLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.text = "°C"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 46, weight: .bold)
+        return label
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        label.text = "London"
+        label.font = .systemFont(ofSize: 22, weight: .medium)
+        return label
+    }()
+    
+    private lazy var searchTextField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.placeholder = "Search"
+        field.font = .systemFont(ofSize: 22, weight: .medium)
+        field.textColor = .systemGray2
+        field.borderStyle = .roundedRect
+        return field
+    }()
+    
+    private lazy var locationPressed: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "location.circle"), for: .normal)
+        button.tintColor = .black
+//        button.backgroundColor = .white
+        button.layer.cornerRadius = 20
+        button.addTarget(self, action: #selector(locationButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var searchPressed: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        button.addTarget(self, action: #selector(searchButtonPressed), for: .touchUpInside)
+//        button.backgroundColor = .white
+        button.tintColor = .black
+        button.layer.cornerRadius = 20
+
+        return button
+    }()
+    
+    //MARK: - Life Cycle
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        searchTextField.delegate = self
+        weatherManager.delegate = self
+    }
+    
+//    @IBAction func locationPressed(_ sender: UIButton) {
+//        locationManager.requestLocation()
+//    }
+    
+    //MARK: - Private Methods
+
+   private func setupUI() {
+       view.addSubview(bgView)
+        view.addSubview(conditionImageView)
+        view.addSubview(temperatureLabel)
+       view.addSubview(cLabel)
+        view.addSubview(cityLabel)
+        view.addSubview(searchTextField)
+        view.addSubview(locationPressed)
+        view.addSubview(searchPressed)
+    }
+    private func setupConstraints() {
+        bgView.topAnchor.constraint(equalTo: view.topAnchor)
+            .isActive = true
+        bgView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+            .isActive = true
+        bgView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            .isActive = true
+        bgView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            .isActive = true
+        
+        searchTextField.centerYAnchor.constraint(equalTo: locationPressed.centerYAnchor)
+            .isActive = true
+        searchTextField.leadingAnchor.constraint(equalTo: locationPressed.trailingAnchor)
+            .isActive = true
+        searchTextField.heightAnchor.constraint(equalToConstant: 50)
+            .isActive = true
+        
+        locationPressed.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+            .isActive = true
+        locationPressed.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+            .isActive = true
+        locationPressed.heightAnchor.constraint(equalToConstant: 50)
+            .isActive = true
+        locationPressed.widthAnchor.constraint(equalToConstant: 50)
+            .isActive = true
+        
+        searchPressed.centerYAnchor.constraint(equalTo: searchTextField.centerYAnchor)
+            .isActive = true
+        searchPressed.leadingAnchor.constraint(equalTo: searchTextField.trailingAnchor)
+            .isActive = true
+        searchPressed.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            .isActive = true
+        searchPressed.heightAnchor.constraint(equalToConstant: 50)
+            .isActive = true
+        searchPressed.widthAnchor.constraint(equalToConstant: 50
+        ).isActive = true
+        
+        conditionImageView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 26)
+            .isActive = true
+        conditionImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        conditionImageView.widthAnchor.constraint(equalToConstant: 100)
+            .isActive = true
+        conditionImageView.heightAnchor.constraint(equalToConstant: 120)
+            .isActive = true
+        
+        temperatureLabel.topAnchor.constraint(equalTo: conditionImageView.bottomAnchor, constant: 16)
+            .isActive = true
+        temperatureLabel.trailingAnchor.constraint(equalTo: cLabel.leadingAnchor, constant: -16)
+            .isActive = true
+        
+        cLabel.topAnchor.constraint(equalTo: conditionImageView.bottomAnchor, constant: 16)
+            .isActive = true
+        cLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+            .isActive = true
+                
+        cityLabel.topAnchor.constraint(equalTo: temperatureLabel.bottomAnchor, constant: 26)
+            .isActive = true
+        cityLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+            .isActive = true
+    }
+    
+    //MARK: - Event Handler
+
+    @objc private func locationButtonPressed() {
+        locationManager.requestLocation()
+    }
+}
+
+//MARK: - Extension UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
+//    @IBAction func searchPressed(_ sender: UIButton) {
+//        searchTextField.endEditing(true)
+//    }
+    @objc private func searchButtonPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type some city"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = "  "
+    }
+}
+
+//MARK: - Extension WeatherManagerDelegate
+
+extension WeatherViewController:  WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error.localizedDescription )
+    }
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
+
+
+
+
---
+++ Climat/Model/WeatherManager.swift
@@ -0,0 +1,84 @@
+//
+//  WeatherManager.swift
+//  Climat
+//
+import UIKit
+import CoreLocation
+
+//MARK: - Protocol WeatherManagerDelegate
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager {
+    
+    //MARK: - Properties
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=04405b54121d2705cff98d43af341e67&units=metric"
+    var delegate: WeatherManagerDelegate?
+    
+    //MARK: - Methods
+
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitude: CLLocationDegrees ,longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        
+        // 1. Create URL
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error)  in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let weather = self.parseJson(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJson(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let temp = decodedData.main.temp
+            let description = decodedData.weather[0].description
+            let id = decodedData.weather[0].id
+            let name = decodedData.name
+            
+            let weather = WeatherModel(
+                conditionId: id,
+                cityName: name,
+                temperature: temp,
+                description: description
+            )
+            print(weather.conditionName, weather.temperatureString, description)
+            return weather
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+}
+
